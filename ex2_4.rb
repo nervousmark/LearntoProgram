b81fb0c5ea390333726b0cee40a2986749343789
Marwan Rateb <mrateb@gmail.com>
2013-06-17 23:48:15 -0400

    Exercises 2.4

---
+++ ex2_4.rb
@@ -0,0 +1,11 @@
+puts "hours in a year: "
+puts 365.0 * 24.0
+
+puts "minutes in a decade: "
+puts 10.0 * 365.0 * 24.0 * 60.0
+
+puts "how many seconds old am I: "
+puts 33.0 * 365.0 * 24.0 * 60.0 * 60.0
+
+puts "how many years in 912M seconds: "
+puts ((((912000000.0 / 60.0 ) / 60.0 )/ 24.0 ) / 365.0)
