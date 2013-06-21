387d483a08aad183075ffdbb8c555ebb4ac7f816
Marwan Rateb <mrateb@gmail.com>
2013-06-20 22:59:11 -0400

    /Users/drateb/LearntoProgram/calc2.rb /Users/drateb/LearntoProgram/calc3.rb

---
+++ ch5_4a.rb
@@ -0,0 +1,7 @@
+puts 'What\'s your first name?'
+first_name = gets.chomp
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+puts 'What\'s your last name?'
+last_name = gets.chomp
+puts 'Greetings ' + first_name + ' ' + middle_name + ' ' + last_name
---
+++ ch5_4b.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+num = gets.to_i
+num = num.to_i + 1
+puts 'How you like dem apples: ' + num.to_s
